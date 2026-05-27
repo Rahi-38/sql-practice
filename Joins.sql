ed199ace7ea1aabc1cc879c58eb1a73b68fd6550
Rahisha <rahishamrafiq@gmail.com>
2026-05-27 19:53:25 +0530

    Updated SQL joins practice queries

--- Joins.sql
+++ Joins.sql
@@ -1,1 +1,19 @@
-sql 
+--Get employee name and their department name.
+
+SELECT 
+    e.first_name,
+    e.last_name,
+    d.department_name
+FROM employees e
+INNER JOIN departments d
+ON e.department_id = d.department_id;
+
+--Show all employees and their department names (even if department is missing).
+SELECT 
+    e.first_name,
+    e.last_name,
+    d.department_name
+FROM employees e
+LEFT JOIN departments d
+ON e.department_id = d.department_id;
+
